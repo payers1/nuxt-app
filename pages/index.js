646a00e310ad712e5f2f9c130060cf9795389349
Phill Ayers <phillip.ayers@sharpnotions.com>
2017-06-26 21:21:01 -0400

    create helpers file for api methods

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,13 @@
-import {mapState, mapMutations} from 'vuex'
-import axios from 'axios'
+import { mapState, mapActions } from 'vuex'
 import List from '~components/List'
-
-const get = (url) => axios({
-  url,
-  method: 'GET',
-  headers: {
-    Authorization: `Basic ${process.env.GITHUB_TOKEN || process.env.github_token || ""}`
-  }
-});
+import { getData } from '../store/helpers';
 
 export default {
   components: {
     List
   },
 
-  async fetch({store, redirect}) {
-    const randomOffset = Math.floor(Math.random(0) * 100);
-    const resp = await get('https://api.github.com/users?per_page=5&since=' + randomOffset)
-
-    const users = resp.data.map(async user => {
-      const { data } = await get(user.url);
-      return data;
-    })
-    const d = await Promise.all(users);
-    store.commit('init', d)
-  },
+  fetch: getData,
 
   computed: {
     ...mapState({
