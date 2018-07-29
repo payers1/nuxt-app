2d126255e03728f5acc621e4cbaff6c9af645c7c
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-07-29 10:36:21 -0400

    linting

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-  css: [
-    '~assets/css/main.css',
-    // 'semantic-ui-css/semantic.css'
-  ],
+  css: ['~assets/css/main.css'],
   build: {
     vendor: ['axios']
   },
-  plugins: [
-    { src: '~/plugins/plugins.js' }
-  ],
+  plugins: [{ src: '~/plugins/plugins.js' }],
   loading: { color: '#3B8070' }
 }
--- plugins/plugins.js
+++ plugins/plugins.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
-
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en'
 import Trend from 'vuetrend'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueRx from 'vue-rx'
+import { Observable } from 'rxjs'
 
-Vue.use(ElementUI)
 Vue.use(Trend)
+Vue.use(ElementUI, { locale })
+Vue.use(VueAxios, axios)
+Vue.use(VueRx, { Observable })
